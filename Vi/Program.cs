d440ee7200818b4edb9597b3d025eb58fc286b48
xQx <xQxCPMxQx@gmail.com>
2014-10-27 19:00:55 +0300

    Fixed Flash+Q Combo

--- Vi/Program.cs
+++ Vi/Program.cs
@@ -75,7 +75,7 @@ namespace Vi
 
             Config.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
             Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalking"));
-            Orbwalker.SetAttacks(true);
+            Orbwalker.SetAttack(true);
 
             // Combo
             Config.AddSubMenu(new Menu("Combo", "Combo"));
