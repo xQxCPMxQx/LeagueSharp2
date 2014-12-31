696a1dfd140f2da4a67cde7c6c852addc80a8629
xQx <xQxCPMxQx@gmail.com>
2014-12-31 11:32:22 +0200

    Update

--- JunglePosition/Program.cs
+++ JunglePosition/Program.cs
@@ -50,18 +50,18 @@ namespace JunglePosition
                 }
                 else if (Game.MapId == GameMapId.SummonersRift)
                 {
-                    Utility.DrawCircle(new Vector3(7444.86f, 2980.26f, 56.26f), circleRange, show.Color);
-                    Utility.DrawCircle(new Vector3(7232.57f, 4671.71f, 51.95f), circleRange, show.Color);
-                    Utility.DrawCircle(new Vector3(7232.57f, 4671.71f, 55.25f), circleRange, show.Color);
-                    Utility.DrawCircle(new Vector3(3402.31f, 8429.14f, 53.79f), circleRange, show.Color);
-                    Utility.DrawCircle(new Vector3(6859.18f, 11497.25f, 52.69f), circleRange, show.Color);
-                    Utility.DrawCircle(new Vector3(7010.90f, 10021.69f, 57.37f), circleRange, show.Color);
-                    Utility.DrawCircle(new Vector3(9850.36f, 8781.23f, 52.63f), circleRange, show.Color);
-                    Utility.DrawCircle(new Vector3(11128.29f, 6225.54f, 54.85f), circleRange, show.Color);
-                    Utility.DrawCircle(new Vector3(10270.61f, 4974.52f, 54f), circleRange, show.Color);
-                    Utility.DrawCircle(new Vector3(7213.78f, 2103.27f, 54.74f), circleRange, show.Color);
-                    Utility.DrawCircle(new Vector3(4142.55f, 5695.95f, 55.26f), circleRange, show.Color);
-                    Utility.DrawCircle(new Vector3(6905.46f, 12402.21f, 53.68f), circleRange, show.Color); 
+                    Utility.DrawCircle(new Vector3(7444.86f, 2980.26f, 56.26f), circleRange, Color.Blue);
+                    Utility.DrawCircle(new Vector3(7232.57f, 4671.71f, 51.95f), circleRange, Color.Blue);
+                    Utility.DrawCircle(new Vector3(7232.57f, 4671.71f, 55.25f), circleRange, Color.Blue);
+                    Utility.DrawCircle(new Vector3(3402.31f, 8429.14f, 53.79f), circleRange, Color.Blue);
+                    Utility.DrawCircle(new Vector3(6859.18f, 11497.25f, 52.69f), circleRange, Color.Blue);
+                    Utility.DrawCircle(new Vector3(7010.90f, 10021.69f, 57.37f), circleRange, Color.Blue);
+                    Utility.DrawCircle(new Vector3(9850.36f, 8781.23f, 52.63f), circleRange, Color.Blue);
+                    Utility.DrawCircle(new Vector3(11128.29f, 6225.54f, 54.85f), circleRange, Color.Blue);
+                    Utility.DrawCircle(new Vector3(10270.61f, 4974.52f, 54f), circleRange, Color.Blue);
+                    Utility.DrawCircle(new Vector3(7213.78f, 2103.27f, 54.74f), circleRange, Color.Blue);
+                    Utility.DrawCircle(new Vector3(4142.55f, 5695.95f, 55.26f), circleRange, Color.Blue);
+                    Utility.DrawCircle(new Vector3(6905.46f, 12402.21f, 53.68f), circleRange, Color.Blue); 
                 }
             }
         }
