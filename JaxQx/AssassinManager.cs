42f292e39e0f2976009a9e8fb67f2b058cd3732f
xQx <xQxCPMxQx@gmail.com>
2015-08-20 10:28:55 +0300

    Update AssassinManager.cs

--- JaxQx/AssassinManager.cs
+++ JaxQx/AssassinManager.cs
@@ -71,7 +71,7 @@ namespace JaxQx
             LocalMenu.AddItem(new MenuItem("Enemies.SearchRange", MenuTab + "Enemy Searching Range"))
                 .SetValue(new Slider(1000, 1500));
 
-            LocalMenu.AddItem(new MenuItem("Enemies.Enemies.Title", "Enemies:", false, MenuItemFontStyle.Bold));
+            LocalMenu.AddItem(new MenuItem("Enemies.Enemies.Title", "Enemies:"));
             {
                 foreach (var enemy in HeroManager.Enemies)
                 {
@@ -79,7 +79,7 @@ namespace JaxQx
                 }
             }
 
-            LocalMenu.AddItem(new MenuItem("Enemies.Other.Title", "Other Settings:", false, MenuItemFontStyle.Bold));
+            LocalMenu.AddItem(new MenuItem("Enemies.Other.Title", "Other Settings:"));
             {
                 LocalMenu.AddItem(
                     new MenuItem("Enemies.AutoPriority Focus", MenuTab + "Auto arrange priorities").SetShared().SetValue(false))
@@ -88,7 +88,7 @@ namespace JaxQx
             LocalMenu.AddItem(
                 new MenuItem("Enemies.Click", MenuTab + "Chance Enemy's Hitchance with Mouse Left-click").SetValue(false));
 
-            LocalMenu.AddItem(new MenuItem("Draw.Title", "Drawings", false, MenuItemFontStyle.Bold));
+            LocalMenu.AddItem(new MenuItem("Draw.Title", "Drawings", false));
             {
                 LocalMenu.AddItem(
                     new MenuItem("Draw.Search", MenuTab + "Show Search Range").SetValue(new Circle(true,
