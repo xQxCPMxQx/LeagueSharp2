ddf3cbf8bb42cfea001dc7790efde48fc5693cb8
xQx <xQxCPMxQx@gmail.com>
2016-11-11 21:35:50 +0200

    updated passive control [6.22]

--- Leblanc II/Common/CommonBuffs.cs
+++ Leblanc II/Common/CommonBuffs.cs
@@ -46,7 +46,9 @@ namespace Leblanc.Common
 
         public static bool HasMarkedWithQ(this Obj_AI_Base obj)
         {
-            return obj.HasBuff("leblancchaosorb");
+            var buff = obj.Buffs.Find(b => b.Name.Equals("Leblancpmark", StringComparison.InvariantCultureIgnoreCase));
+            return buff != null && buff.StartTime + 1.5 < Game.Time;
+            //return obj.HasBuff("leblancchaosorb");
         }
 
         public static bool HasSheenBuff(this Obj_AI_Base obj)
